a5585b723df311d06d67885cb1d5164053c1c699
Kirill <lainekirill@mail.ru>
2026-03-30 17:47:40 +0300

    configure frontend for render backend

--- web/assets/api/config.js
+++ web/assets/api/config.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = window.__API_BASE_URL__ || '/api';
+// export const API_BASE_URL = 'https://onlinechat-6mxm.onrender.com' || '/api';
+export const API_BASE_URL =
+  window.__API_BASE_URL__ || 'https://onlinechat-6mxm.onrender.com';
